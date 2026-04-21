b9525c74aa4114796d7b2ed077c9698458f1cc6f
alevy <andrei.levchenko.ie@gmail.com>
2026-04-21 22:36:29 +0100

    Update home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,51 +1,70 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonItem, IonLabel, IonSelect, IonSelectOption,
+  IonSpinner
+} from '@ionic/angular/standalone';
 import { COUNTIES, County } from '../shared/counties';
 import { OverpassService } from '../services/overpass-service';
+import { Station } from '../models/station.model';
+import { StationCardComponent } from '../components/station-card/station-card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption],
+  imports: [
+    CommonModule, FormsModule,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonItem, IonLabel, IonSelect, IonSelectOption,
+    IonSpinner, StationCardComponent
+  ],
 })
 export class HomePage {
-
   counties: County[] = COUNTIES;
   selectedCounties: string[] = [];
-  results: any[] = [];
+  results: Station[] = [];
+  loading = false;
 
-  constructor(private overpassService: OverpassService) { }
-
-  ngOnInit() {
-    this.loadData();
-
-  }
+  constructor(private overpassService: OverpassService) {}
 
   onCountyChange() {
-    // this.selectedCounties = event.detail.value;
-    console.log('Chosen:', this.selectedCounties);
     this.loadData();
   }
 
   private loadData() {
+    if (this.selectedCounties.length === 0) return;
 
-    if (this.selectedCounties.length === 0) {
-      return;
-    }
+    this.loading = true;
 
-    this.overpassService
-      .getAmenitiesByCounties(this.selectedCounties)
-      .subscribe({
-        next: (response) => {
-          console.log('Overpass response:', response);
-          this.results = response.elements;
-        },
-        error: (err) => {
-          console.error('Overpass error:', err);
-        }
-      });
+    this.loadFromJsonBlob();
+    return;
+    this.overpassService.getStationsByCounties(this.selectedCounties).subscribe({
+      next: (stations) => {
+        this.results = stations;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Overpass API error:', err);
+        this.loading = false;
+        this.loadFromJsonBlob();
+      }
+    });
   }
 
+  private loadFromJsonBlob() {
+    this.loading = true;
+    this.overpassService.getStationsFromJsonBlob().subscribe({
+      next: (stations) => {
+        this.results = stations;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error loading from JSON blob:', err);
+        this.loading = false;
+      }
+    });
+  }
 }
