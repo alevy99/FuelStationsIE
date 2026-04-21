c6e2bec000d3b58acae34ad533188cb0cf0654cc
alevy <andrei.levchenko.ie@gmail.com>
2026-04-21 16:15:20 +0100

    add counties constant data

---
+++ src/app/shared/counties.ts
@@ -0,0 +1,34 @@
+export interface County {
+  value: string;
+  label: string;
+  osmName: string;
+}
+
+export const COUNTIES: County[] = [
+  { value: 'carlow', label: 'Carlow', osmName: 'County Carlow' },
+  { value: 'cavan', label: 'Cavan', osmName: 'County Cavan' },
+  { value: 'clare', label: 'Clare', osmName: 'County Clare' },
+  { value: 'cork', label: 'Cork', osmName: 'County Cork' },
+  { value: 'donegal', label: 'Donegal', osmName: 'County Donegal' },
+  { value: 'dublin', label: 'Dublin', osmName: 'County Dublin' },
+  { value: 'galway', label: 'Galway', osmName: 'County Galway' },
+  { value: 'kerry', label: 'Kerry', osmName: 'County Kerry' },
+  { value: 'kildare', label: 'Kildare', osmName: 'County Kildare' },
+  { value: 'kilkenny', label: 'Kilkenny', osmName: 'County Kilkenny' },
+  { value: 'laois', label: 'Laois', osmName: 'County Laois' },
+  { value: 'leitrim', label: 'Leitrim', osmName: 'County Leitrim' },
+  { value: 'limerick', label: 'Limerick', osmName: 'County Limerick' },
+  { value: 'longford', label: 'Longford', osmName: 'County Longford' },
+  { value: 'louth', label: 'Louth', osmName: 'County Louth' },
+  { value: 'mayo', label: 'Mayo', osmName: 'County Mayo' },
+  { value: 'meath', label: 'Meath', osmName: 'County Meath' },
+  { value: 'monaghan', label: 'Monaghan', osmName: 'County Monaghan' },
+  { value: 'offaly', label: 'Offaly', osmName: 'County Offaly' },
+  { value: 'roscommon', label: 'Roscommon', osmName: 'County Roscommon' },
+  { value: 'sligo', label: 'Sligo', osmName: 'County Sligo' },
+  { value: 'tipperary', label: 'Tipperary', osmName: 'County Tipperary' },
+  { value: 'waterford', label: 'Waterford', osmName: 'County Waterford' },
+  { value: 'westmeath', label: 'Westmeath', osmName: 'County Westmeath' },
+  { value: 'wexford', label: 'Wexford', osmName: 'County Wexford' },
+  { value: 'wicklow', label: 'Wicklow', osmName: 'County Wicklow' }
+];
