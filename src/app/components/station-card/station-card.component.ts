f329d2e0eb2fb617b744d5b0085f8e3a1195640b
alevy <andrei.levchenko.ie@gmail.com>
2026-04-23 00:23:45 +0100

    Station card - add fuel price and UI improvements

--- src/app/components/station-card/station-card.component.ts
+++ src/app/components/station-card/station-card.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonCard, IonCardContent, IonBadge } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { Station } from '../../models/station.model';
 import { getBrandLogoURL } from 'src/app/shared/station-utils';
+import { FuelPrice } from '../../services/fuel-prices-service';
 
 @Component({
   selector: 'app-station-card',
   templateUrl: './station-card.component.html',
   styleUrls: ['./station-card.component.scss'],
-  imports: [CommonModule, IonCard, IonCardContent, IonBadge],
+  imports: [CommonModule, IonCard, IonCardContent],
 })
 export class StationCardComponent {
-  @Input() station !: Station;
+  @Input() station!: Station;
+  @Input() fuelPrice?: FuelPrice;
 
   constructor(private router: Router) {}
 
